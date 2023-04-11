3e7c2e8b914fd69106f919dcfabfce8e26bdcaa4
Ravier Gardon <130451025+kero-py@users.noreply.github.com>
2023-04-11 12:19:01 +0100

    Update chromedemo1.py
    deleted Keys import as unused

--- chromedemo1.py
+++ chromedemo1.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
